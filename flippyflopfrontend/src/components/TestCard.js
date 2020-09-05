ba1aebb5503a973525ce6dbf8bf11ed647e8d79c
Jacques Donnelly <jacquesdonnelly@hotmail.co.uk>
2020-09-05 11:13:19 +0100

    alignment of card and buttons

--- flippyflopfrontend/src/components/TestCard.js
+++ flippyflopfrontend/src/components/TestCard.js
@@ -1,36 +1,35 @@
 import React from 'react';
-import Container from 'react-bootstrap/Container'
-import Row from 'react-bootstrap/Row'
-import Col from 'react-bootstrap/Col'
 import Button from 'react-bootstrap/Button'
 import './TestCard.css'
 
-const card = (
-	<div className="Card">
-		Hello, World!
-	</div>
-)
-
+function Card(props) {
+	return (
+		<div className="Card">
+			{props.front}
+		</div>
+	)
+}
 
 // TODO: 
-// Make sure the button and card are always the correct size for each screen width
+// Move Card to its own component file
 // Change the background color to dark grey
 // Style the card (centering text) and make buttons look nice (color correctly)
-// Center the TestCard component on the page
+// Center (vertically using flex) the TestCard component on the page
+// Make sure the layout is appropirate for mobile
+
+
 
 function TestCard(props) {
+	const card = {
+		"front": "This is the front of my card!",
+		"back": "You can't see this yet!"
+	}
 	return (
-		<Container fluid>
-			<Row className="justify-content-center someMargin">
-				<Col md="auto">{card}</Col>
-			</Row>
-			<Row className="justify-content-center someMargin">
-				<Button className="resultButton">Easy Peasy Lemon Squeazy!</Button>
-			</Row>
-			<Row className="justify-content-center someMargin">
-				<Button className="resultButton">Stressed Depressed Lemon Zest!</Button>
-			</Row>
-		</Container>
+		<div className="testCardContainer">
+			<Card front={card.front}/>
+			<Button className="resultButton">Easy Peasy Lemon Squeazy!</Button>
+			<Button className="resultButton">Stressed Depressed Lemon Zest!</Button>
+		</div>
 	)
 }
 
