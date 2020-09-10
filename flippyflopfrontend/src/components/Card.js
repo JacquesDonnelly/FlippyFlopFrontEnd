8c34de6a60ba5029e02905983d6f06bd4a19970b
Jacques Donnelly <jacquesdonnelly@hotmail.co.uk>
2020-09-10 11:24:44 +0100

    the card now flips

--- flippyflopfrontend/src/components/Card.js
+++ flippyflopfrontend/src/components/Card.js
@@ -1,12 +1,33 @@
 import React from 'react';
 import './Card.css'
 
-function Card(props) {
-	return (
-		<div className="Card">
-			{props.front}
-		</div>
-	)
+
+class Card extends React.Component {
+	constructor(props) {
+		super(props);
+		this.flip = this.flip.bind(this)
+		this.state = {is_flipped: false};
+	}
+
+	flip() {
+		const priorFlipState = this.state.is_flipped;
+		this.setState({is_flipped: !priorFlipState});
+	}
+
+	render() {
+		const flippedClass = (this.state.is_flipped ? 'flipped' : '')
+		const flippyThingClass = `flippything ${flippedClass}`
+	
+		return (
+			<div className="flipCard">
+				<div className={flippyThingClass} onClick={this.flip}>
+					<div className="side front">Front of my Card</div>
+					<div className="side back"> Back of my Card</div>
+				</div>
+			</div>
+		)
+	}
 }
+
 
 export default Card
